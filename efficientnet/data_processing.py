34e41a4e5bc241debb33819e95f0052a44558582
Toby <lhgiang149@gmail.com>
2020-04-06 18:10:10 +0700

    temp

--- efficientnet/data_processing.py
+++ efficientnet/data_processing.py
@@ -15,74 +15,14 @@ import imgaug as ia
 import imgaug.augmenters as iaa
 
 def augument(images, labels, number_aug = 2):
-    sometimes = lambda aug: iaa.Sometimes(0.5, aug)
-    seq = iaa.Sequential(
-        [
+    seq = iaa.Sequential([
+    iaa.Affine(rotate=(-50, 50)),
+    iaa.Crop(percent=(0, 0.2))], random_order=True)
 
-            iaa.Fliplr(0.5), 
-            iaa.Flipud(0.2), 
-
-            sometimes(iaa.CropAndPad(
-                percent=(-0.05, 0.1),
-                pad_mode=ia.ALL,
-                pad_cval=(0, 255)
-            )),
-            sometimes(iaa.Affine(
-                scale={"x": (0.8, 1.2), "y": (0.8, 1.2)}, 
-                translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)}, 
-                rotate=(-45, 45), 
-                shear=(-16, 16), 
-                order=[0, 1], 
-                cval=(0, 255), 
-                mode=ia.ALL 
-            )),
-
-            iaa.SomeOf((0, 5),
-                [
-                    sometimes(iaa.Superpixels(p_replace=(0, 1.0), n_segments=(20, 200))), 
-                    iaa.OneOf([
-                        iaa.GaussianBlur((0, 3.0)), 
-                        iaa.AverageBlur(k=(2, 7)), 
-                        iaa.MedianBlur(k=(3, 11)), 
-                    ]),
-                    iaa.Sharpen(alpha=(0, 1.0), lightness=(0.75, 1.5)), 
-                    iaa.Emboss(alpha=(0, 1.0), strength=(0, 2.0)), 
-                    iaa.SimplexNoiseAlpha(iaa.OneOf([
-                        iaa.EdgeDetect(alpha=(0.5, 1.0)),
-                        iaa.DirectedEdgeDetect(alpha=(0.5, 1.0), direction=(0.0, 1.0)),
-                    ])),
-                    iaa.AdditiveGaussianNoise(loc=0, scale=(0.0, 0.05*255), per_channel=0.5), 
-                    iaa.OneOf([
-                        iaa.Dropout((0.01, 0.1), per_channel=0.5),
-                        iaa.CoarseDropout((0.03, 0.15), size_percent=(0.02, 0.05), per_channel=0.2),
-                    ]),
-                    iaa.Invert(0.05, per_channel=True), 
-                    iaa.Add((-10, 10), per_channel=0.5), 
-                    iaa.AddToHueAndSaturation((-20, 20)), 
-                    iaa.OneOf([
-                        iaa.Multiply((0.5, 1.5), per_channel=0.5),
-                        iaa.FrequencyNoiseAlpha(
-                            exponent=(-4, 0),
-                            first=iaa.Multiply((0.5, 1.5), per_channel=True),
-                            second=iaa.LinearContrast((0.5, 2.0))
-                        )
-                    ]),
-                    iaa.LinearContrast((0.5, 2.0), per_channel=0.5), 
-                    iaa.Grayscale(alpha=(0.0, 1.0)),
-                    sometimes(iaa.ElasticTransformation(alpha=(0.5, 3.5), sigma=0.25)), 
-                    sometimes(iaa.PiecewiseAffine(scale=(0.01, 0.05))), 
-                    sometimes(iaa.PerspectiveTransform(scale=(0.01, 0.1)))
-                ],
-                random_order=True
-            )
-        ],
-        random_order=True
-    )
     temp_image = np.copy(images)
     temp_labels = np.copy(labels)
     for _ in range(number_aug):
         image_aug = seq(images=images)
-        # images_aug = np.vstack(seq(images=images))
         temp_image = np.concatenate((temp_image,image_aug))
         temp_labels = np.concatenate((temp_labels,labels))
         
@@ -103,25 +43,12 @@ def letterbox_image(image, size):
 def readAndProcess(image_path):
     path = image_path
     im = Image.open(path)
-    im = im.resize((600,600), Image.BICUBIC)
-    im = np.array(im)
-    # im = np.array(letterbox_image(im, (600,600)))
+    # im = im.resize((600,600), Image.BICUBIC)
+    # im = np.array(im)
+    im = np.array(letterbox_image(im, (600,600)))
     # im = np.reshape(im,[1]+list(im.shape))
     return im 
     
-
-def multithread_preprocess_data(image_path, csv_path, executor):
-    csv = pd.read_csv(csv_path)
-    labels = True if csv.shape[1] == 5 else False
-    path = []
-    for image in csv['image_id']:
-        path.append(image_path + image + '.jpg')
-    X = np.vstack(executor.map(readAndProcess,path))
-    if labels:
-        y = csv.loc[:, 'healthy':].values
-        return X,y
-    else:
-        return X
 
 def unison_shuffled_copies(x, y):
     assert len(x) == len(y)
@@ -142,16 +69,13 @@ def train_generator(image_dir, train, batch_size, y, num_train):
             image = readAndProcess(image_dir + str(train[num]) + '.jpg')
             image_data.append(image)
             i = (i+1)%num_train
-
-        image_data = np.array(image_data)
-        image_data = image_data/255
+        # image_data = np.array(image_data)
         y_true = np.copy(y[base:i])
         if len(y_true) != len(image_data):
             y_true = np.copy(y[base:len(y)])
             y_true = np.concatenate((y_true,np.copy(y[:i]))) 
-        # data = ImageDataGenerator()
-        # image_data, y_true = augument(image_data, y_true)
-        # print(image_data.shape, y_true.shape)
+        image_data, y_true = augument(image_data, y_true)
+        image_data = image_data/255
         yield [image_data, y_true]
         
 
@@ -171,5 +95,6 @@ def val_generator(image_dir, val, batch_size, y, num_val):
         if len(y_true) != len(image_data):
             y_true = np.copy(y[base:len(y)])
             y_true = np.concatenate((y_true,np.copy(y[:i])))
-        # print(image_data.shape, y_true.shape,i)
         yield [image_data, y_true]
+
+
--- efficientnet/main.py
+++ efficientnet/main.py
@@ -20,6 +20,7 @@ from keras.layers import Activation
 from keras import metrics
 
 from keras.preprocessing.image import ImageDataGenerator
+from sklearn.model_selection import train_test_split
 
 get_custom_objects().update({'swish': Activation(swish)})
 
@@ -31,21 +32,29 @@ def _main():
     csv = pd.read_csv(labels_path)
     image_named = np.array(csv['image_id'])
     y = csv.loc[:, 'healthy':].values
-    image_named , y = unison_shuffled_copies(image_named, y)
-    image_named = list(image_named)
+    # image_named , y = unison_shuffled_copies(image_named, y)
+    # image_named = list(image_named)
     
-    num_train = 1500
-    train = image_named[:num_train]
-    val = image_named[num_train:]
-    y_train = np.copy(y[:num_train])
-    y_val = np.copy(y[num_train:])
-    
+    # num_train = 1500
+    # train = image_named[:num_train]
+    # val = image_named[num_train:]
+    # y_train = np.copy(y[:num_train])
+    # y_val = np.copy(y[num_train:])
+    train ,val, y_train,y_val = train_test_split(image_named, y, test_size = 0.22, random_state = 42, shuffle = True)
 
-    freeze = 3
+    datagen = ImageDataGenerator(
+    featurewise_center=True,
+    featurewise_std_normalization=True,
+    rotation_range=20,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    horizontal_flip=True)
+
+    freeze = 1
 
     # step = 607
-    num_train = 1500
-    num_val = 321
+    num_train = 1420
+    num_val = 401
     
     log_dir = 'model/log/'
     weights_path = 'model/model.h5'
@@ -57,39 +66,33 @@ def _main():
                         models = keras.models,
                         utils = keras.utils,
                         include_top=True)
-    base_model.layers.pop()
-    base_model.outputs = [base_model.layers[-1].output]
+    
     output = keras.layers.Dense(4, activation='softmax', 
                         kernel_initializer=DENSE_KERNEL_INITIALIZER,
-                        name='probs_1')(base_model.output)
+                        name='probs')(base_model.layers[-2].output)
     model = keras.models.Model(inputs = [base_model.input], outputs=[output])
-    
     # # temporary use Adam and
     adam = Adam(lr = 0.001)
     # logging = TensorBoard(log_dir=log_dir)
     checkpoint = ModelCheckpoint(log_dir + 'ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5',
         monitor='val_loss', save_weights_only=True, save_best_only=True, mode = 'min')
-    # mcp_save = keras.callbacks.callbacks.ModelCheckpoint('.mdl_wts.hdf5', 
-    #                                                 save_best_only=True, 
-    #                                                 monitor='val_loss', 
-    #                                                 mode='min')
-
+   
 
 
     # Freeze FC layers
     for i in range((len(model.layers)-freeze)):
         model.layers[i].trainable = False
+
     # model.compile(optimizer = adam, 
     #             loss = 'categorical_crossentropy',
     #             # loss = [categorical_focal_loss(alpha=.25, gamma=2)],
     #             metrics = ['accuracy'])
     model.compile(optimizer = adam, 
             loss = 'categorical_crossentropy',
-            # loss = [categorical_focal_loss(alpha=.25, gamma=2)],
-            metrics = [metrics.categorical_accuracy])
+            metrics = ['accuracy'])
 
     batch_size  = 6
-
+    
     model.fit_generator(train_generator(image_path, train, batch_size, y_train, num_train),
         epochs = 20, 
         steps_per_epoch = num_train//batch_size, 
@@ -99,14 +102,7 @@ def _main():
         verbose = 1,
         callbacks=[checkpoint])
     model.save_weights(log_dir+'model.h5')
-    # model.fit_generator(aug.flow(X,y,batch_size = 20), epochs = 20, step_per_epoch = num_train//batch_size  , metrics = ['accuracy'])
-
-# def focal_loss(gamma=2., alpha=.25):
-# 	def focal_loss_fixed(y_true, y_pred):
-# 		pt_1 = tf.where(tf.equal(y_true, 1), y_pred, tf.ones_like(y_pred))
-# 		pt_0 = tf.where(tf.equal(y_true, 0), y_pred, tf.zeros_like(y_pred))
-# 		return -K.mean(alpha * K.pow(1. - pt_1, gamma) * K.log(pt_1)) - K.mean((1 - alpha) * K.pow(pt_0, gamma) * K.log(1. - pt_0))
-# 	return focal_loss_fixed
+   
 def categorical_focal_loss(gamma=2., alpha=.25):
     def categorical_focal_loss_fixed(y_true, y_pred):
         # Scale predictions so that the class probas of each sample sum to 1
